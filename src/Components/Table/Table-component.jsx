61da4dd0e37cbfb0c2744332f45e4d57745e3c4d
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-08-19 20:27:03 -0400

    fuck this

--- src/Components/Table/Table-component.jsx
+++ src/Components/Table/Table-component.jsx
@@ -21,49 +21,51 @@ const Table = () => {
   const { filteredEntries } = useContext(SearchContext);
 
   return (
-    <div>
-      {/* headers */}
-      <div className='header-container'>
-        {HEADER_TITLES.map((title, index) => {
-          return (
-            <div className='table-header' key={index}>
-              <div className='table-icon'>{headerIcons.alphabet}</div>
-              <span className='table-label'>{title}</span>
-            </div>
-          );
-        })}
-      </div>
-
-      <div className='body-container'>
-        {/* Entries */}
-        {filteredEntries.map(entry => {
-          return (
-            <div className='table-body' key={entry.id}>
-              <div className='table-body-company'>
-                {bodyIcons.pageIcon}
-                <span>{entry.company}</span>
+    <>
+      <div>
+        {/* headers */}
+        <div className='header-container'>
+          {HEADER_TITLES.map((title, index) => {
+            return (
+              <div className='table-header' key={index}>
+                <div className='table-icon'>{headerIcons.alphabet}</div>
+                <span className='table-label'>{title}</span>
               </div>
-              <EditableElement>
-                <div>{entry.stage}</div>
-              </EditableElement>
-              <div>{entry.interviewDate}</div>
-              <div>{entry.position}</div>
-              <div>{entry.location}</div>
-              <div>{entry.postingURL}</div>
-              <div>{entry.pointOfContact}</div>
-              <div>{entry.contactInfo}</div>
-              <div>{entry.dateApplied}</div>
-            </div>
-          );
-        })}
-        <div className='count-container'>
-          <span>
-            COUNT
-            <CountEntries entries={filteredEntries} />
-          </span>
+            );
+          })}
+        </div>
+
+        {/* Entries */}
+        <div className='body-container'>
+          {filteredEntries.map(entry => {
+            return (
+              <div className='table-body' key={entry.id}>
+                <div className='table-body-company'>
+                  {bodyIcons.pageIcon}
+                  <span>{entry.company}</span>
+                </div>
+                <EditableElement>
+                  <div>{entry.stage}</div>
+                </EditableElement>
+                <div>{entry.interviewDate}</div>
+                <div>{entry.position}</div>
+                <div>{entry.location}</div>
+                <div>{entry.postingURL}</div>
+                <div>{entry.pointOfContact}</div>
+                <div>{entry.contactInfo}</div>
+                <div>{entry.dateApplied}</div>
+              </div>
+            );
+          })}
+          <div className='count-container'>
+            <span>
+              COUNT
+              <CountEntries entries={filteredEntries} />
+            </span>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
